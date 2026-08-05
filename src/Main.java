ce386f7f5a4e3902fdf6984f9cfbc4b37424c925
Pedro Neves <pedro24319245@gmail.com>
2026-08-05 17:40:35 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+
+        System.out.println("Ano de lançamento: " + ano);
+        boolean incluidoNoPlano = false;
     }
 }
